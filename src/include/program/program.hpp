7be2f655f285be86490bcc1eff5955bde3f18ff9
Tigran Bantikyan <59093401+tbantikyan@users.noreply.github.com>
2025-07-13 15:53:29 -0700

    feat: threading support code (#7)
    * feat: defined ASSERT and FATAL
    
    * Merge branch 'main' into feat-threading
    
    * style: add punctuation to docstring
    
    * feat: add threading support functions

---
+++ src/include/program/program.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+/*
+ * program.hpp
+ * Utility functions that can operate of and on the flow of the program.
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+inline auto ASSERT(bool cond, const std::string &msg) noexcept {
+    if (!cond) [[unlikely]] {
+        std::cerr << msg << '\n';
+        exit(EXIT_FAILURE);
+    }
+}
+
+inline auto FATAL(const std::string &msg) noexcept {
+    std::cerr << msg << '\n';
+    exit(EXIT_FAILURE);
+}
