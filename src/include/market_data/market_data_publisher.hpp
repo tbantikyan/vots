767473f86e6a2b3722b5ec1bd1e08027dd6a9f2d
Tigran Bantikyan <59093401+tbantikyan@users.noreply.github.com>
2025-08-01 21:58:52 -0700

    feat: snapshot synthesizer & market data publisher declaration (#27)

---
+++ src/include/market_data/market_data_publisher.hpp
@@ -0,0 +1,77 @@
+/*
+ * market_data_publisher.hpp
+ * The MarketDataPublisher is responsible for publishing public market updates for market participants to follow along
+ * with trade executions and order modifications. It publishes all updates to on a UDP multicast socket called the
+ * incremental stream; UDP helps achieve the ultra low-latency need of sharing updates with participants as quickly as
+ * possible. To allow participants to synchronize with the trading exchange, the market data publisher also compiles
+ * data and occasionally pushes a large snapshot via the snapshot stream.
+ */
+
+#pragma once
+
+#include <functional>
+
+#include "snapshot_synthesizer.hpp"
+
+namespace exchange {
+class MarketDataPublisher {
+   public:
+    MarketDataPublisher(MEMarketUpdateLFQueue *market_updates, const std::string &iface, const std::string &snapshot_ip,
+                        int snapshot_port, const std::string &incremental_ip, int incremental_port);
+
+    ~MarketDataPublisher() {
+        Stop();
+
+        using namespace std::literals::chrono_literals;
+        std::this_thread::sleep_for(5s);
+
+        delete snapshot_synthesizer_;
+        snapshot_synthesizer_ = nullptr;
+    }
+
+    void Run() noexcept;
+
+    void Start() {
+        run_ = true;
+
+        ASSERT(common::CreateAndStartThread(-1, "Exchange/MarketDataPublisher", [this]() { Run(); }) != nullptr,
+               "Failed to start MarketData thread.");
+
+        snapshot_synthesizer_->Start();
+    }
+
+    void Stop() {
+        run_ = false;
+
+        snapshot_synthesizer_->Stop();
+    }
+
+    // Deleted default, copy & move constructors and assignment-operators.
+    MarketDataPublisher() = delete;
+
+    MarketDataPublisher(const MarketDataPublisher &) = delete;
+
+    MarketDataPublisher(const MarketDataPublisher &&) = delete;
+
+    auto operator=(const MarketDataPublisher &) -> MarketDataPublisher & = delete;
+
+    auto operator=(const MarketDataPublisher &&) -> MarketDataPublisher & = delete;
+
+   private:
+    size_t next_inc_seq_num_ = 1;
+    MEMarketUpdateLFQueue *outgoing_md_updates_ = nullptr;
+
+    MDPMarketUpdateLFQueue snapshot_md_updates_;
+
+    volatile bool run_ = false;
+
+    std::string time_str_;
+
+    common::Logger logger_;
+
+    common::McastSocket incremental_socket_;
+
+    SnapshotSynthesizer *snapshot_synthesizer_ = nullptr;
+};
+
+}  // namespace exchange
--- src/include/market_data/market_update.hpp
+++ src/include/market_data/market_update.hpp
@@ -1,6 +1,6 @@
 /*
  * market_update.hpp
- * Defines struct representing a market updaete for the matching engine to forward to the market data publisher and
+ * Defines struct representing a market update for the matching engine to forward to the market data publisher and
  * lock-free queue type for the communication channel.
  */
 
@@ -14,10 +14,21 @@
 namespace exchange {
 
 #pragma pack(push, 1)
-enum class MarketUpdateType : uint8_t { INVALID = 0, ADD = 1, MODIFY = 2, CANCEL = 3, TRADE = 4 };
+enum class MarketUpdateType : uint8_t {
+    INVALID = 0,
+    CLEAR = 1,
+    ADD = 2,
+    MODIFY = 3,
+    CANCEL = 4,
+    TRADE = 5,
+    SNAPSHOT_START = 6,
+    SNAPSHOT_END = 7
+};
 
 inline auto MarketUpdateTypeToString(MarketUpdateType type) -> std::string {
     switch (type) {
+        case MarketUpdateType::CLEAR:
+            return "CLEAR";
         case MarketUpdateType::ADD:
             return "ADD";
         case MarketUpdateType::MODIFY:
@@ -26,6 +37,10 @@ inline auto MarketUpdateTypeToString(MarketUpdateType type) -> std::string {
             return "CANCEL";
         case MarketUpdateType::TRADE:
             return "TRADE";
+        case MarketUpdateType::SNAPSHOT_START:
+            return "SNAPSHOT_START";
+        case MarketUpdateType::SNAPSHOT_END:
+            return "SNAPSHOT_END";
         case MarketUpdateType::INVALID:
             return "INVALID";
     }
@@ -54,8 +69,22 @@ struct MEMarketUpdate {
     }
 };
 
+// MDPMarketUpdate is the type of an incremental market update in the public market data protocol.
+struct MDPMarketUpdate {
+    size_t seq_num_ = 0;
+    MEMarketUpdate me_market_update_;
+
+    auto ToString() const {
+        std::stringstream ss;
+        ss << "MDPMarketUpdate"
+           << " ["
+           << " seq:" << seq_num_ << " " << me_market_update_.ToString() << "]";
+        return ss.str();
+    }
+};
 #pragma pack(pop)
 
 using MEMarketUpdateLFQueue = common::LockFreeQueue<MEMarketUpdate>;
+using MDPMarketUpdateLFQueue = common::LockFreeQueue<MDPMarketUpdate>;
 
 }  // namespace exchange
