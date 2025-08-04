05a6895c5c27b8e33d7c1519194a0eeeecabc9e5
Tigran Bantikyan <tigranbantikyan@gmail.com>
2025-08-04 03:55:46 +0000

    feat: update time utils

--- src/include/common/time_utils.hpp
+++ src/include/common/time_utils.hpp
@@ -25,12 +25,14 @@ inline auto GetCurrentNanos() noexcept {
 }
 
 inline auto GetCurrentTimeStr(std::string *time_str) -> auto & {
-    using std::chrono::system_clock;
-    const auto time = system_clock::to_time_t(system_clock::now());
-    time_str->assign(ctime(&time));
-    if (!time_str->empty()) {
-        time_str->at(time_str->length() - 1) = '\0';  // replace new line character with null terminator
-    }
+    const auto clock = std::chrono::system_clock::now();
+    const auto time = std::chrono::system_clock::to_time_t(clock);
+
+    char nanos_str[24];
+    sprintf(nanos_str, "%.8s.%09ld", ctime(&time) + 11,
+            std::chrono::duration_cast<std::chrono::nanoseconds>(clock.time_since_epoch()).count() % NANOS_TO_SECS);
+    time_str->assign(nanos_str);
+
     return *time_str;
 }
 
