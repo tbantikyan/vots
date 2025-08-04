9d00d4e2b0396742933ba649750caa3ec1eb93fe
Tigran Bantikyan <tigranbantikyan@gmail.com>
2025-08-04 04:52:01 +0000

    fix: use ARM register

--- src/include/common/perf_utils.hpp
+++ src/include/common/perf_utils.hpp
@@ -1,6 +1,6 @@
 /*
  * perf_utils.hpp
- * Defines utility method using assembly rdtsc register to read CPU cycle count. Defines associated utility macros.
+ * Defines utility method using an assembly register to read CPU cycle count. Defines associated utility macros.
  */
 
 #pragma once
@@ -10,23 +10,22 @@
 namespace common {
 
 // Read from the TSC register and return a uint64_t value to represent elapsed CPU clock cycles.
-inline auto rdtsc() noexcept {  // NOLINT
-    unsigned int lo;
-    unsigned int hi;
-    __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
-    return (static_cast<uint64_t>(hi) << 32) | lo;
+inline auto CycleCount() noexcept {  
+    uint64_t cnt;
+    asm volatile("mrs %0, CNTVCT_EL0" : "=r"(cnt));
+    return cnt;
 }
 }  // namespace common
 
-// Start latency measurement using rdtsc(). Creates a variable called TAG in the local scope.
-#define START_MEASURE(TAG) const auto TAG = common::rdtsc()
+// Start latency measurement using CycleCount(). Creates a variable called TAG in the local scope.
+#define START_MEASURE(TAG) const auto TAG = common::CycleCount()
 
-// End latency measurement using rdtsc(). Expects a variable called TAG to already exist in the local scope.
+// End latency measurement using CycleCount(). Expects a variable called TAG to already exist in the local scope.
 // Do while forces user to add ; at the end of macro use.
 #define END_MEASURE(TAG, LOGGER)                                                                    \
     do {                                                                                            \
-        const auto end = common::rdtsc();                                                           \
-        (LOGGER).Log("% RDTSC " #TAG " %\n", common::GetCurrentTimeStr(&time_str_), (end - (TAG))); \
+        const auto end = common::CycleCount();                                                           \
+        (LOGGER).Log("% Cycle Count " #TAG " %\n", common::GetCurrentTimeStr(&time_str_), (end - (TAG))); \
     } while (false)
 
 // Log a current timestamp at the time this macro is invoked.
