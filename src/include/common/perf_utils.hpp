3ca90085f756bd839a4587a376887b4dbbc6d369
Tigran Bantikyan <tigranbantikyan@gmail.com>
2025-08-04 04:30:50 +0000

    fix: syntax issues in Macros

--- src/include/common/perf_utils.hpp
+++ src/include/common/perf_utils.hpp
@@ -19,19 +19,19 @@ inline auto rdtsc() noexcept {  // NOLINT
 }  // namespace common
 
 // Start latency measurement using rdtsc(). Creates a variable called TAG in the local scope.
-#define START_MEASURE(TAG) const auto TAG = Common::rdtsc()
+#define START_MEASURE(TAG) const auto TAG = common::rdtsc()
 
 // End latency measurement using rdtsc(). Expects a variable called TAG to already exist in the local scope.
 // Do while forces user to add ; at the end of macro use.
 #define END_MEASURE(TAG, LOGGER)                                                                    \
     do {                                                                                            \
-        const auto end = Common::rdtsc();                                                           \
-        (LOGGER).log("% RDTSC " #TAG " %\n", Common::getCurrentTimeStr(&time_str_), (end - (TAG))); \
+        const auto end = common::rdtsc();                                                           \
+        (LOGGER).Log("% RDTSC " #TAG " %\n", common::GetCurrentTimeStr(&time_str_), (end - (TAG))); \
     } while (false)
 
 // Log a current timestamp at the time this macro is invoked.
 #define TTT_MEASURE(TAG, LOGGER)                                                        \
     do {                                                                                \
-        const auto(TAG) = Common::getCurrentNanos();                                    \
-        (LOGGER).log("% TTT " #TAG " %\n", Common::getCurrentTimeStr(&time_str_), TAG); \
+        const auto(TAG) = common::GetCurrentNanos();                                    \
+        (LOGGER).Log("% TTT " #TAG " %\n", common::GetCurrentTimeStr(&time_str_), TAG); \
     } while (false)
