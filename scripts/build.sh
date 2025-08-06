a2307528b50ccae64dbcbcbd194b17beced5a1ff
Tigran Bantikyan <59093401+tbantikyan@users.noreply.github.com>
2025-08-05 18:57:07 -0700

    perf: release build in script (#55)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 cd ../build \
-    && cmake .. -DCMAKE_TOOLCHAIN_FILE=/vcpkg/scripts/buildsystems/vcpkg.cmake \
+    && cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/vcpkg/scripts/buildsystems/vcpkg.cmake \
     && make exchange_main \
     && make trading_main
 
